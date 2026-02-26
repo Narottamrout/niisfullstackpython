6d7df67e25ba9543b0f631cafed6792504424604
Narottamrout <narottamrout224@gamil.com>
2026-02-27 00:08:43 +0530

    test

---
+++ pythonniis/function/even.py
@@ -0,0 +1,10 @@
+def even():
+	n=int(input("enter number"))
+	if n%2==0:
+		print("even")
+	else:
+		print("odd")
+
+even()
+even()
+even()			
---
+++ pythonniis/function/sum.py
@@ -0,0 +1,14 @@
+# def sum():
+# 	a=5
+# 		b=5
+# 	sum=a+b
+# 	print(sum)
+# sum()
+# def add():
+# 	a=int(input("enter first number"))
+# 	b=int(input("enter second number"))
+# 	sum=a+b
+# 	print(sum)
+
+# add()	
+
