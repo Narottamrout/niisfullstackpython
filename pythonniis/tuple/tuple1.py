2ceeec617fdb04924144d2c4e82d067f1dc4d6f4
Narottamrout <narottamrout224@gamil.com>
2026-03-23 00:35:48 +0530

    test

---
+++ pythonniis/tuple/tuple1.py
@@ -0,0 +1,7 @@
+#display value in tuple
+t=10,"hi",2.5
+# for i in t:
+# 	print(i)
+#using range
+for i in range(len(t)):
+	print(t[i])
---
+++ pythonniis/tuple/tuple2.py
@@ -0,0 +1,6 @@
+#pre defind function in tuple
+#index
+t=(10,"hi",2.5,"bye")
+print(t.index(2.5))
+#count
+print(t.count(10))
